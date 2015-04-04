0dff33950a70599e508c68c1d7e98ad7c67262a1
steve-mm <smmbuy@yahoo.com>
2015-04-04 19:37:07 -0400

    walkthrough

--- js/app.js
+++ js/app.js
@@ -5,6 +5,15 @@ $(document).ready(function() {
   })
   .mouseleave(function() {
     $('.ryu-ready').hide();
-	$('.ryu-still').show();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    console.log('mousedown');
+    // play hadouken sound
+    // show hadouken and animate it to the right of the screen
+  })
+  .mouseup(function() {
+    console.log('mouseup');
+    // ryu goes back to his ready position
   });
 });
