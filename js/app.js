06c5c3a7776665b81c25f8d457f31ff4c041e7c4
steve-mm <smmbuy@yahoo.com>
2015-04-04 19:34:17 -0400

    leave

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
-	$('.ryu-still').hide();
-	$('.ryu-ready').show();
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+	$('.ryu-still').show();
+  });
 });
